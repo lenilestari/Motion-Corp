a14c00f33c63fff6d2372473e8e6483be5546081
lenilestari <lenilestari>
2023-10-18 11:25:50 +0700

    Penghapusan header dan footer pada home

--- app/src/main/java/com/example/motioncorp/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/motioncorp/Fragment/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.motioncorp.Fragment
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,12 +14,15 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.example.motioncorp.R
 import com.example.motioncorp.databinding.FragmentHomeBinding
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.io.IOException
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-
+    private val url = "https://motioncorpbymmtc.id/"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,7 +34,6 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         return root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -40,46 +44,55 @@ class HomeFragment : Fragment() {
                 url: String
             ): Boolean {
                 view.loadUrl(url)
-
-                view.loadUrl("javascript:(function() { " +
-                        "document.getElementById('header').style.display='none'; " +
-                        "})()");
-
                 return true
-
-
             }
-
-
         }
         val webSetting: WebSettings = myWebView.settings
         webSetting.javaScriptEnabled = true
         myWebView.webViewClient = WebViewClient()
 
         myWebView.canGoBack()
-        myWebView.setOnKeyListener(View.OnKeyListener { v , keyCode, event ->
+        myWebView.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_BACK
-
                 && event.action == MotionEvent.ACTION_UP
-                && myWebView.canGoBack()){
+                && myWebView.canGoBack()
+            ) {
                 myWebView.goBack()
                 return@OnKeyListener true
             }
             false
         })
 
-
-        myWebView.loadUrl("https://motioncorpbymmtc.id/")
-        myWebView.settings.javaScriptEnabled = true
-        myWebView.settings.allowContentAccess = true
-        myWebView.settings.domStorageEnabled = true
-        myWebView.settings.useWideViewPort = true
-
-
+        // Menggunakan AsyncTask untuk mengambil dan memproses HTML
+        MyAsyncTask(myWebView).execute(url)
     }
 
+    private inner class MyAsyncTask(private val webView: WebView) : AsyncTask<String, Void, String>() {
+        override fun doInBackground(vararg urls: String): String? {
+            val url = urls[0]
+            var document: Document? = null
+            try {
+                document = Jsoup.connect(url).get()
+                document.getElementsByClass("elementor elementor-867 elementor-location-header").remove()
+                document.getElementsByClass("elementor elementor-880 elementor-location-footer").remove()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            return document?.toString()
+        }
+
+        override fun onPostExecute(html: String?) {
+            super.onPostExecute(html)
+            if (html != null) {
+                val modifiedHtml = "<html>$html</html>"
+                webView.loadDataWithBaseURL(url, modifiedHtml, "text/html", "utf-8", "")
+                webView.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
-
-
-
-
