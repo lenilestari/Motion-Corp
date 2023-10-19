00ea26cfbe3ef1c37fcedd5e406ce42960f5a1f8
lenilestari <lenilestari>
2023-10-19 09:38:58 +0700

    Penghapusan header dan footer pada Televisi

--- app/src/main/java/com/example/motioncorp/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/motioncorp/Fragment/HomeFragment.kt
@@ -2,7 +2,6 @@ package com.example.motioncorp.Fragment
 
 import android.os.AsyncTask
 import android.os.Bundle
-import android.provider.DocumentsContract
 import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
--- app/src/main/java/com/example/motioncorp/Fragment/TelevisiFragment.kt
+++ app/src/main/java/com/example/motioncorp/Fragment/TelevisiFragment.kt
@@ -1,5 +1,6 @@
 package com.example.motioncorp.Fragment
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.view.KeyEvent
 import androidx.fragment.app.Fragment
@@ -12,21 +13,27 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.example.motioncorp.R
 import com.example.motioncorp.databinding.FragmentTelevisiBinding
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.io.IOException
 
 class TelevisiFragment : Fragment() {
 
-    private var _binding : FragmentTelevisiBinding? = null
+    private var _binding: FragmentTelevisiBinding? = null
     private val binding get() = _binding!!
-
+    private val url1 = "https://tv.motioncorpbymmtc.id/"
+    private val url2 = "https://tv.motioncorpbymmtc.id/motion-tv-live/"
+    private var currentUrl: String = url1 // Menyimpan URL saat ini
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+    ): View {
 
-    ): View? {
         _binding = FragmentTelevisiBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
         return root
     }
 
@@ -34,46 +41,67 @@ class TelevisiFragment : Fragment() {
         val myWebView: WebView = view.findViewById(R.id.WebView2)
         myWebView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(
-                view: WebView,
-                url: String
+                view: WebView?,
+                url: String?
             ): Boolean {
-                view.loadUrl(url)
-
-                view.loadUrl("javascript:(function() { " +
-                        "document.getElementById('header').style.display='none'; " +
-                        "})()");
-
+                if (url == "https://tv.motioncorpbymmtc.id/index.php/motion-tv-live/") {
+                    // Klik tombol "Stream Video," akses URL baru (https://tv.motioncorpbymmtc.id/motion-tv-live/)
+                    MyAsyncTask(myWebView).execute(url2)
+                    return true
+                }
+                currentUrl = url1 // Saat navigasi ke URL baru, simpan URL saat ini
+                view?.loadUrl(url1)
                 return true
-
-
             }
-
-
         }
+
         val webSetting: WebSettings = myWebView.settings
         webSetting.javaScriptEnabled = true
-        myWebView.webViewClient = WebViewClient()
 
         myWebView.canGoBack()
-        myWebView.setOnKeyListener(View.OnKeyListener { v , keyCode, event ->
+        myWebView.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_BACK
-
                 && event.action == MotionEvent.ACTION_UP
-                && myWebView.canGoBack()){
+                && myWebView.canGoBack()
+            ) {
                 myWebView.goBack()
                 return@OnKeyListener true
             }
             false
         })
 
-
-        myWebView.loadUrl("https://tv.motioncorpbymmtc.id/")
-        myWebView.settings.javaScriptEnabled = true
-        myWebView.settings.allowContentAccess = true
-        myWebView.settings.domStorageEnabled = true
-        myWebView.settings.useWideViewPort = true
-
-
+        // Menggunakan AsyncTask untuk mengambil dan memproses HTML
+        MyAsyncTask(myWebView).execute(url1)
     }
 
+    private inner class MyAsyncTask(private val webView: WebView) : AsyncTask<String, Void, String>() {
+        override fun doInBackground(vararg urls: String): String? {
+            val url = urls[0]
+            var document: Document? = null
+            try {
+                document = Jsoup.connect(url).get()
+                document.getElementsByClass("elementor elementor-24 elementor-location-header").remove()
+                document.getElementsByClass("elementor elementor-40 elementor-location-footer").remove()
+                document.getElementsByClass("elementor elementor-132 elementor-location-header").remove()
+                document.getElementsByClass("elementor elementor-40 elementor-location-footer").remove()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            return document?.toString()
+        }
+
+        override fun onPostExecute(html: String?) {
+            super.onPostExecute(html)
+            if (html != null) {
+                val modifiedHtml = "<html>$html</html>"
+                webView.loadDataWithBaseURL(currentUrl, modifiedHtml, "text/html", "utf-8", "")
+                webView.settings.cacheMode = WebSettings.LOAD_CACHE_ELSE_NETWORK
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
